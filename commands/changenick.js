4344888c73ac37541ab43bfc506252d4d8f843d3
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-11-12 20:07:45 +0000

    ⚡️ add change nick command

---
+++ commands/changenick.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "changenick",
+    description: "Change your nickname.",
+    execute(message, args) {
+        if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
+        message.member.setNickname(message.content.replace('changeNick ', ''));
+    }
+};

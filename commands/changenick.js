8d073279b5ef39275ac85eb5c411f1987117ba2c
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-11-13 21:04:29 -0600

    make no argument to reset nick

--- commands/changenick.js
+++ commands/changenick.js
@@ -3,6 +3,7 @@ module.exports = {
     description: "Change your nickname.",
     execute(message, args) {
         if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
-        message.member.setNickname(args[0]);
+        if (!message.content.length) ? return message.member.setNickname() : message.member.setNickname(args[0]);
+        message.delete();
     }
 };

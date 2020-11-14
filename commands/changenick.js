3a0633aa7e820a01aec28e44233dc0c6253e05f0
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-11-13 22:58:04 -0600

    fix the ternary fix from the previous commit, ugh

--- commands/changenick.js
+++ commands/changenick.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "Change your nickname.",
     execute(message, args) {
         if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
-        message.content.length ? return message.member.setNickname(args[0]) : message.member.setNickname();
+        message.content.length ? message.member.setNickname(args[0]) : message.member.setNickname();
         message.delete();
     }
 };
